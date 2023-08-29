f9e6a3d2f8ea16bb7a3bf1cfff03b64713a7ee2f
lagino <boxlagin@gmail.com>
2023-08-29 20:55:48 +0300

    added_for_break

--- 03_loops.js
+++ 03_loops.js
@@ -1,4 +1,4 @@
-let num = 10
+// let num = 10
 // while (num <= 7) {
 //     console.log(num)
 //     num++
@@ -15,3 +15,12 @@ let num = 10
 //     console.log(num)
 // } while (num <= 7)
 
+
+let sumOfloop = 0;
+
+for (let i = 0; i < 7; i+=2) {
+    console.log(i)
+    if (i ==6 ) break;
+    sumOfloop += i
+}
+console.log(sumOfloop)
