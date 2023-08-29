152dfdb0e05285cb52a81845dbfbd46eaff618f1
lagino <boxlagin@gmail.com>
2023-08-29 20:45:45 +0300

    added_wile_loop

---
+++ 03_loops.js
@@ -0,0 +1,17 @@
+let num = 10
+// while (num <= 7) {
+//     console.log(num)
+//     num++
+// }
+
+//do while
+// do {
+//     console.log(num)
+//     num++
+// } while (num <= 7)
+
+// do {
+//     num++
+//     console.log(num)
+// } while (num <= 7)
+
