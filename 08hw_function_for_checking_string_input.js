03859fc548e38306642af9b7f3a36dc326441a57
lagino <boxlagin@gmail.com>
2023-09-24 14:13:36 +0300

    hw8

---
+++ 08hw_function_for_checking_string_input.js
@@ -0,0 +1,18 @@
+// Create a function that receives one parameter.
+// Check if a given string has all symbols in upper case. 
+//If the string is empty or doesn't have any letter in it - the function should return false.
+
+function isUpCase(str) {
+    if (!str) {
+    return false;
+    }
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (/[a-zA-Z]/.test(char)) {
+            if (char !== char.isUpCase()) {
+            return false;
+            }
+        }
+    }
+    return true;
+    }
